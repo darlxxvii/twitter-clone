fdc1505607cadcddd8e386780d6b4beb733530a5
darlxxvii <jekenn9@gmail.com>
2025-03-24 23:07:18 +0500

    auth-controller added - 1

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { signup } from '../controllers/auth.controller.js';
 import { login } from '../controllers/auth.controller.js';
-import { logout } from '../controllers/auth.controller.js';
+import { logout, getMe } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middleware/protect.route.js';
 
 const router = express.Router();
 
+router.get("/me", protectRoute, getMe);
 router.post("/signup", signup);
-
 router.post("/login", login);
-
 router.post("/logout", logout)
+
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); //to parse req.body
+app.use(express.urlencoded({ extended: true })); //to parse from data
+
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
+
+app.get('/', (req, res) => {
+    res.send('Server is working!'); 
+}); 
+
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
+    console.log(`server is running on http://localhost:${PORT}`);
     connectMongoDB();
 });

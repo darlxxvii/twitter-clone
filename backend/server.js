10fcdeab916e5654a1717e8fbe9f628dcea2efb7
darlxxvii <jekenn9@gmail.com>
2025-04-03 11:14:13 +0500

    create post function added - 3

--- backend/server.js
+++ backend/server.js
@@ -15,12 +15,12 @@ cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
-})
+});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); //to parse req.body
+app.use(express.json({limit: "5mb"})); //to parse req.body
 app.use(express.urlencoded({ extended: true })); //to parse from data
 
 app.use(cookieParser());

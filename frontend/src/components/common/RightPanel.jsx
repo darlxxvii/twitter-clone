dcb734df321a698df8d74a6154f32d641ee8c217
darlxxvii <jekenn9@gmail.com>
2025-04-04 11:06:38 +0500

    useFollow hook added - 3

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,7 +1,9 @@
-import { Link } from "react-router-dom";
-import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
 import {useQuery} from "@tanstack/react-query"
+import { Link } from "react-router-dom";
 
+import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
+import LoadingSpinner from './LoadingSpinner'
+import useFollow from "../../../hooks/useFollow";
 const RightPanel = () => {
 	const {data: suggestedUsers, isLoading} = useQuery({
 		queryKey: ["suggestedUsers"],
@@ -18,6 +20,8 @@ const RightPanel = () => {
 			}
 		}
 	});
+	const { follow, isPending } = useFollow();
+	
 	if(suggestedUsers?.length === 0) return <div className="md:w-64 w-0"></div>
 
 	return (
@@ -57,9 +61,12 @@ const RightPanel = () => {
 								<div>
 									<button
 										className='btn bg-white text-black hover:bg-white hover:opacity-90 rounded-full btn-sm'
-										onClick={(e) => e.preventDefault()}
+										onClick={(e) => {
+											e.preventDefault();
+											follow(user._id)
+										}}
 									>
-										Follow
+										{isPending ? <LoadingSpinner size='sm'/> : "Follow"}
 									</button>
 								</div>
 							</Link>

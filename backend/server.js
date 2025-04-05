f56f752c00ce834258c51f869413d407c995e6c3
darlxxvii <jekenn9@gmail.com>
2025-04-05 11:02:51 +0500

    prepared our app fr]or deployment - 4

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 import cors from 'cors';
@@ -21,6 +22,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve()
 
 // Middleware
 app.use(express.json({ limit: "10mb" })); 
@@ -32,6 +34,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+}
+
+app.get("*", (req, res) => {
+    res.send(path.resolve(__dirname, "frontend", "dist", "index.html"));
+})
 
 app.get('/', (req, res) => {
     res.send('Server is working!'); 
